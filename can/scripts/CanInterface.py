bd2f314717b0ad7839c43dc2d709d3e5e3bbb9ee
Mathias Neerup <manee12@student.sdu.dk>
2015-10-22 16:38:10 +0200

    Splitting into smaller files

---
+++ can/scripts/CanInterface.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import can
+
+__author__ = 'Mathias Neerup'
+class CanInterface:
+        def __init__(self, iface, type):
+                self.bus = can.interface.Bus(iface, bustype=type)
+
+        def send(self,extended_id, payload):
+                msg = can.Message(arbitration_id=extended_id, data=payload, extended_id=True)
+                print msg
+                if self.bus.send(msg) < 0:
+                        exit("Unable to send, stopping")
+                else:
+                        print("Message sent")
+
+
+        def recv(self):
+                return self.bus.recv(timeout = 1)
