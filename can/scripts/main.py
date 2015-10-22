95fdd07eb8ef1f8616f64cf52b45c67f2f4eaf7b
Mathias Neerup <manee12@student.sdu.dk>
2015-10-22 16:54:23 +0200

    Send hex as data, easier when debugging

--- can/scripts/main.py
+++ can/scripts/main.py
@@ -10,56 +10,35 @@ sudo pip install python-can
 You will need to change the name of the CAN-interface to make it work.
 If you can't find the name of the interface in 'ifconfig', you might be missing loading one or more kernel-modules (pcan, vcan ect. It depends on our CAN hardware)
 '''
-import time
-import can
-
-
-class CanInterface:
-        def __init__(self):
-                self.bus = can.interface.Bus('can0', bustype='socketcan')
-
-        def send(self,extended_id, payload):
-                msg = can.Message(arbitration_id=extended_id, data=payload, extended_id=True)
-                if self.bus.send(msg) < 0:
-                        exit("Unable to send, stopping")
-                else:
-                        print("Message sent")
-
-
-        def recv(self):
-                return self.bus.recv(timeout = 1)
-
+from struct import unpack
+from CanInterface import CanInterface
 
 
 class AutoQuadNode(CanInterface):
-    def __init__(self):
+    def __init__(self, iface, type):
         # Call base constructor
-        CanInterface.__init__(self)
+        CanInterface.__init__(self, iface, type)
 
     def RegisterNode(self):
         # ...1c... = register node, can.h
-        self.send(0x01c00000, 8*'0')
+        self.send(0x01c00000, unpack('<BBBBBBBB','\x01\x23\x45\x67\x89\xAB\xCD\xEF'))
 
         # Get ACK msg (if any)
         msg = self.recv()
-        return 1 if msg else 0
+
+        #Throws exception if msg is None = we haven't received an msg
+        assert(msg != None)
+
+        return msg
 
 
 if __name__ == "__main__":
-    '''
-                # Instantiate CanInterface
-                canInterface = CanInterface()
+        autoquadNode = AutoQuadNode('can0', 'socketcan')
 
-                # Create new threads
-                thread1 = CanReceiverThread(canInterface)
-
-                # Start new Threads
-                thread1.start()
-
-                # Register as node in AQ
-                canInterface.send(0x01c00000, 'A'*8)
-                time.sleep(1)
-                print "Exiting main()"
-    '''
-    autoquadNode = AutoQuadNode()
-    autoquadNode.RegisterNode()
+    try:
+        print "Registering node"
+        rsp = autoquadNode.RegisterNode()
+        print "Registration complete"
+        print rsp
+    except AssertionError:
+        exit("Unable to register node")
