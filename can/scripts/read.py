d77495c93c51cccdcd0efb3d4fc652ea395b71f2
Mathias Neerup <manee12@student.sdu.dk>
2015-10-22 15:36:46 +0200

    Simple example on how to use socketcan(python module)

--- can/scripts/read.py
+++ can/scripts/read.py
@@ -2,26 +2,25 @@
 
 import can
 import time
-import can
-import time
-
 
 class CAN:
 	def __init__(self):
-		self.bus = can.interface.Bus('slcan1', bustype='socketcan')
-
+		self.bus = can.interface.Bus('slcan0', bustype='socketcan')
 
 	def send(self,data):
-		msg = can.Message(arbitration_id=0x000000, data=data, extended_id=False)
-		return (self.bus.send(msg) < 0)
+		msg = can.Message(arbitration_id=0x1e000000, data=data, extended_id=False)
+		if self.bus.send(msg) < 0:
+			print("Message NOT sent")
+		else:
+			print("Message sent")
 
 
 	def recv(self):
 		return self.bus.recv()
 
 if __name__ == "__main__":
-	canz = CAN()
-	while True:
-		print (canz.recv())
-
-
+		canz = CAN()
+#	while True:
+#		print (canz.recv())
+		canz.send('AAAAAAAA')
+		time.sleep(0.5)
